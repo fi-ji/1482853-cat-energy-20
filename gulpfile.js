b7aa60c53ebf7b18127447ec7316e9a30bdcbcc0
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-07-22 15:00:38 +0200

    исправляет табы в файлах

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ const sync = require('browser-sync').create();
 // Styles
 
 const styles = () => {
-	return gulp
-		.src('source/sass/style.scss')
-		.pipe(plumber())
-		.pipe(sourcemap.init())
-		.pipe(sass())
-		.pipe(postcss([ autoprefixer() ]))
-		.pipe(sourcemap.write('.'))
-		.pipe(gulp.dest('source/css'))
-		.pipe(sync.stream());
+  return gulp
+    .src('source/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('source/css'))
+    .pipe(sync.stream());
 };
 
 exports.styles = styles;
@@ -25,15 +25,15 @@ exports.styles = styles;
 // Server
 
 const server = (done) => {
-	sync.init({
-		server: {
-			baseDir: 'source'
-		},
-		cors: true,
-		notify: false,
-		ui: false
-	});
-	done();
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false
+  });
+  done();
 };
 
 exports.server = server;
@@ -41,8 +41,8 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-	gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
-	gulp.watch('source/*.html').on('change', sync.reload);
+  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('source/*.html').on('change', sync.reload);
 };
 
 exports.default = gulp.series(styles, server, watcher);
