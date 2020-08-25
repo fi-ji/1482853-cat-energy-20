f8352e4759b67c51192694ae8203bbb95be5fa0e
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-08-25 18:32:47 +0200

    обновляет css для переполнения контентом

--- source/js/script.js
+++ source/js/script.js
@@ -20,123 +20,127 @@ headerToggle.addEventListener('click', function () {
 var tabletWidth = 768;
 var desktopWidth = 1300;
 
+var pageIndex = document.querySelector('#page-index');
 var example = document.querySelector('.example');
-var sliderImgBefore = example.querySelector('.example__img-wrapper--before');
-var sliderImgAfter = example.querySelector('.example__img-wrapper--after');
-var sliderScale = example.querySelector('.example__scale');
-var sliderBar = example.querySelector('.example__bar');
-var sliderToggle = example.querySelector('.example__toggle');
-var btnBefore = example.querySelector('.example__slider-button--before');
-var btnAfter = example.querySelector('.example__slider-button--after');
+var sliderImgBefore = document.querySelector('.example__img-wrapper--before');
+var sliderImgAfter = document.querySelector('.example__img-wrapper--after');
+var sliderScale = document.querySelector('.example__scale');
+var sliderBar = document.querySelector('.example__bar');
+var sliderToggle = document.querySelector('.example__toggle');
+var btnBefore = document.querySelector('.example__slider-button--before');
+var btnAfter = document.querySelector('.example__slider-button--after');
 var toggleWidth, scaleWidth, sliderWidth;
 
-function getElemWidth(elem) {
-  return parseInt(getComputedStyle(elem).width, 10);
-}
+if (pageIndex) {
 
-var getElemCoords = function (elem) {
-  var box = elem.getBoundingClientRect();
-  return box.left + pageXOffset;
-};
-
-btnBefore.addEventListener('click', function (e) {
-  e.preventDefault();
-  sliderImgBefore.style.width = '100%';
-  sliderImgAfter.style.width = '0';
-  sliderToggle.style.left = '0';
-  sliderBar.style.marginLeft = '0';
-  sliderBar.style.transition = 'margin-left 2.5s ease-in-out';
-  sliderImgBefore.style.transition = 'width 2s ease-in-out';
-
-  if (viewport >= tabletWidth) {
-    sliderToggle.style.transition = 'left 3s ease-in-out';
-    sliderImgBefore.style.transition = 'width 3s ease-in-out';
+  function getElemWidth(elem) {
+    return parseInt(getComputedStyle(elem).width, 10);
   }
-});
 
-btnAfter.addEventListener('click', function (e) {
-  e.preventDefault();
-  sliderImgBefore.style.width = '0';
-  sliderImgAfter.style.width = '100%';
-  sliderToggle.style.left = 'calc(100% - ' + toggleWidth + 'px)';
-  sliderBar.style.marginLeft = '50%';
-  sliderBar.style.transition = 'margin-left 2.5s ease-in-out';
-  sliderImgAfter.style.transition = 'width 2s ease-in-out';
-
-  if (viewport >= tabletWidth) {
-    sliderToggle.style.transition = 'left 3s ease-in-out';
-    sliderImgAfter.style.transition = 'width 3s ease-in-out';
-  }
-});
-
-sliderToggle.ondblclick = function () {
-  sliderImgBefore.style.width = '50%';
-  sliderImgAfter.style.width = '50%';
-  sliderToggle.style.left = 'calc(50% - 15' + 'px)';
-};
-
-function toggleDownHandler(evtDown) {
-  var toggleCoords = getElemCoords(sliderToggle);
-  var scaleCoords = getElemCoords(sliderScale);
-  sliderToggle.style.transition = 'none';
-
-  var shiftX = evtDown.pageX - toggleCoords;
-
-  document.onmousemove = function (evtMove) {
-    var newLeft = evtMove.pageX - shiftX - scaleCoords;
-
-    if (newLeft < 0) {
-      newLeft = 0;
-    }
-
-    var rightEdge = scaleWidth - toggleWidth;
-    if (newLeft > rightEdge) {
-      newLeft = rightEdge;
-    }
-
-    var toggleValue = newLeft / rightEdge * 100;
-    sliderToggle.style.left = newLeft + 'px';
-
-    sliderImgBefore.style.width = 100 - toggleValue + '%';
-    sliderImgAfter.style.width = toggleValue + '%';
+  var getElemCoords = function (elem) {
+    var box = elem.getBoundingClientRect();
+    return box.left + pageXOffset;
   };
 
-  document.onmouseup = function () {
-    document.onmousemove = document.onmouseup = null;
-    sliderToggle.style.transition = 'left 0.2s ease-out';
+  btnBefore.addEventListener('click', function (e) {
+    e.preventDefault();
+    sliderImgBefore.style.width = '100%';
+    sliderImgAfter.style.width = '0';
+    sliderToggle.style.left = '0';
+    sliderBar.style.marginLeft = '0';
+    sliderBar.style.transition = 'margin-left 2.5s ease-in-out';
+    sliderImgBefore.style.transition = 'width 2s ease-in-out';
+
+    if (viewport >= tabletWidth) {
+      sliderToggle.style.transition = 'left 3s ease-in-out';
+      sliderImgBefore.style.transition = 'width 3s ease-in-out';
+    }
+  });
+
+  btnAfter.addEventListener('click', function (e) {
+    e.preventDefault();
+    sliderImgBefore.style.width = '0';
+    sliderImgAfter.style.width = '100%';
+    sliderToggle.style.left = 'calc(100% - ' + toggleWidth + 'px)';
+    sliderBar.style.marginLeft = '50%';
+    sliderBar.style.transition = 'margin-left 2.5s ease-in-out';
+    sliderImgAfter.style.transition = 'width 2s ease-in-out';
+
+    if (viewport >= tabletWidth) {
+      sliderToggle.style.transition = 'left 3s ease-in-out';
+      sliderImgAfter.style.transition = 'width 3s ease-in-out';
+    }
+  });
+
+  sliderToggle.ondblclick = function () {
+    sliderImgBefore.style.width = '50%';
+    sliderImgAfter.style.width = '50%';
+    sliderToggle.style.left = 'calc(50% - 15' + 'px)';
   };
 
-  return false;
-}
+  function toggleDownHandler(evtDown) {
+    var toggleCoords = getElemCoords(sliderToggle);
+    var scaleCoords = getElemCoords(sliderScale);
+    sliderToggle.style.transition = 'none';
 
-function addToggleHandlers() {
-  sliderToggle.addEventListener('mousedown', toggleDownHandler);
-}
+    var shiftX = evtDown.pageX - toggleCoords;
 
-function removeGripHandlers() {
-  sliderToggle.removeEventListener('mousedown', toggleDownHandler);
-}
+    document.onmousemove = function (evtMove) {
+      var newLeft = evtMove.pageX - shiftX - scaleCoords;
 
-var initialize = function () {
-  viewport = document.documentElement.clientWidth || window.innerWidth;
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
 
-  if (viewport >= tabletWidth) {
-    addToggleHandlers();
-  } else {
-    removeToggleHandlers();
+      var rightEdge = scaleWidth - toggleWidth;
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      var toggleValue = newLeft / rightEdge * 100;
+      sliderToggle.style.left = newLeft + 'px';
+
+      sliderImgBefore.style.width = 100 - toggleValue + '%';
+      sliderImgAfter.style.width = toggleValue + '%';
+    };
+
+    document.onmouseup = function () {
+      document.onmousemove = document.onmouseup = null;
+      sliderToggle.style.transition = 'left 0.2s ease-out';
+    };
+
+    return false;
   }
 
-  sliderWidth = getElemWidth(example);
-  scaleWidth = getElemWidth(sliderScale);
-  toggleWidth = getElemWidth(sliderToggle);
+  function addToggleHandlers() {
+    sliderToggle.addEventListener('mousedown', toggleDownHandler);
+  }
 
-  sliderImgBefore.style.width = '';
-  sliderImgAfter.style.width = '';
-  sliderToggle.style.left = '';
-};
+  function removeGripHandlers() {
+    sliderToggle.removeEventListener('mousedown', toggleDownHandler);
+  }
 
-window.addEventListener('load', initialize);
-window.addEventListener('resize', initialize);
+  var initialize = function () {
+    viewport = document.documentElement.clientWidth || window.innerWidth;
+
+    if (viewport >= tabletWidth) {
+      addToggleHandlers();
+    } else {
+      removeToggleHandlers();
+    }
+
+    sliderWidth = getElemWidth(example);
+    scaleWidth = getElemWidth(sliderScale);
+    toggleWidth = getElemWidth(sliderToggle);
+
+    sliderImgBefore.style.width = '';
+    sliderImgAfter.style.width = '';
+    sliderToggle.style.left = '';
+  };
+
+  window.addEventListener('load', initialize);
+  window.addEventListener('resize', initialize);
+}
 
 // Валидация формы
 
